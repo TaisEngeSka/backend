3d4e5e88ace9fcc8a5139384c1e7f7ce7be1a2e4
Taís Caroline Enge <tais.enge@ska.com.br>
2026-05-27 22:24:44 -0300

    Configurando o Backed

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Servidor Node.js com TypeScript funcionando!");
-});
-
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get("/efetuarLogin", (requisicao, resposta) => {
+  resposta.send("TA FUNCIONANDOOO!!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor Node.js com TypeScript funcionando!");
 });
