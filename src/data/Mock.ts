ea77cf8766a1393667ec5c90bda885dae44cc02f
Taís Caroline Enge <tais.enge@ska.com.br>
2026-05-28 22:36:53 -0300

    atividades com get

---
+++ src/data/Mock.ts
@@ -0,0 +1,17 @@
+export const usuario = [
+    { codigo: 1, 
+    nomeUsuario: "João", 
+    email: "joao@example.com", 
+    senha: "senha123" },
+
+    { codigo: 5, 
+    nomeUsuario: "Maria", 
+    email: "maria@example.com", 
+    senha: "senha456" },
+
+    { codigo: 3, 
+    nomeUsuario: "Pedro", 
+    email: "pedro@example.com", 
+    senha: "senha789" }
+];
+
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { usuario } from "./data/Mock";
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +9,46 @@ app.listen(PORT, () => {
 });
 
 app.get("/efetuarLogin", (requisicao, resposta) => {
-  resposta.send("TA FUNCIONANDOOO!!");
+  const user = {
+    idade: 17,
+    nome: "Taís",
+    sobrenome: "Santos",
+    cpf: "123.456.789-00",
+  };
+
+  return resposta.status(200).json(user);
+});
+ 
+app.get("/ListaUsers", (req, res) => {
+  return res.status(200).json(usuario);
+});
+
+// DELETE OU GET
+app.get("/istaUsers/:codigo", (req, res) => {
+  const codigo = req.params.codigo;
+  return res.status(200).json({mensagem: "VOCÊ DIGITOU " + codigo});
+});
+
+app.get("/Exercicio/:codigo", (req, res) => {
+  const codigo = req.params.codigo;
+
+  if (codigo == null || codigo == ""){
+      return res.status(200).json(usuario);
+  } 
+  
+  for {
+    return res.status(200).json(usuario[codigo - 1]);
+  }
+});
+
+app.get("/exercicio/:codigo", (req, res) => {
+  const codigo = req.params.codigo;
+
+  if (codigo == null || codigo == ""){
+      return res.status(200).json(usuario);
+  } else {
+    return res.status(200).json(usuario[codigo - 1]);
+  }
 });
 
 app.get("/", (req, res) => {
