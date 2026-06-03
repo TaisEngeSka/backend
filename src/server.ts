c8d57bb14ce006774556b54ba47796f24c814c5a
Taís Caroline Enge <tais.enge@ska.com.br>
2026-06-02 21:17:47 -0300

    validação de senha

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }))
 
-app.use(express.json()) 
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
@@ -36,21 +36,45 @@ app.get("/ListaUsers", (req, res) => {
 app.post("/efetuarLogin", (req, res) => {
   const { nomeUser, senhaUser } = req.body;
 
-    if (user === nomeUser && senha === senhaUser){
-      return res.status(200).json({ mensagem: "Login bem-sucedido!" });
-    }
+  if (user === nomeUser && senha === senhaUser) {
+    return res.status(200).json({ mensagem: "Login bem-sucedido!" });
+  }
 
-    return res.status(401).json({ mensagem: "Credenciais inválidas!" });
+  return res.status(401).json({ mensagem: "Credenciais inválidas!" });
 });
 
+// POST
 app.post("/efetuarCadastro", (req, res) => {
   const { email, senhaUser, ConfirmaSenha, nomeUser, foneContato } = req.body;
 
-    if (ConfirmaSenha === senhaUser){
-      return res.status(200).json({ mensagem: "Cadastro realizado com sucesso!" });
-    }
+  // Validar e-mail
+  const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    return res.status(401).json({ mensagem: "senhas não coincidem!" });
+  if (!emailValido.test(email)) {
+    return res.status(400).json({
+      mensagem: "E-mail inválido!"
+    });
+  }
+
+  // Validar senha
+  const senhaValida = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+
+  if (!senhaValida.test(senhaUser)) {
+    return res.status(400).json({
+      mensagem: "A senha deve ter no mínimo 8 caracteres, uma letra e um número."
+    });
+  }
+
+  // Confirmar senha
+  if (ConfirmaSenha !== senhaUser) {
+    return res.status(400).json({
+      mensagem: "As senhas não coincidem!"
+    });
+  }
+
+  return res.status(200).json({
+    mensagem: "Cadastro realizado com sucesso!"
+  });
 });
 
 // DELETE OU GET
@@ -67,10 +91,10 @@ app.get("/Exercicio/:codigo", (req, res) => {
   }
 
   let encontrei;
-  for ( let buscar of usuario ) {
-    if(buscar.codigo == Number(codigo)) { encontrei = buscar }
-}
-    return res.status(200).json(encontrei);
+  for (let buscar of usuario) {
+    if (buscar.codigo == Number(codigo)) { encontrei = buscar }
+  }
+  return res.status(200).json(encontrei);
 
 }
 );
