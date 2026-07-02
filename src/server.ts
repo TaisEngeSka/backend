a1a00a7c38146bce4849b2bca72f9522699fe329
Taís Caroline Enge <tais.enge@ska.com.br>
2026-07-02 19:54:41 -0300

    Limpeza do arquivo server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import express from "express";
-import { cadastros, senhaSalva, user } from "./data/Mock";
 import cors from "cors";
-import { LoginInterface } from "./interfaces/Login";
-import { CadastroInterface } from "./interfaces/Cadastro";
-import { Response } from "express";
-import { EsqueciSenhaInterface } from "./interfaces/EsqueciSenha";
 import autenticacao from "./routes/AuthRoute";
 import ProdutoRoutes from "./routes/PodutoRoutes";
 
@@ -25,56 +20,6 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando e m http://localhost:${PORT}`);
 });
 
-function respostaServidor(res: Response, mensagem: any, status: number) {
-  return res.status(status).json({
-    mensagem: mensagem
-  });
-}
-
-app.post("/efetuarLogin", (req, res) => {
-  const { username, senha }: LoginInterface = req.body;
-
-  if (user != username || senhaSalva != senha) {
-    return respostaServidor(res, "Credenciais inválidas!", 401);
-  }
-
-  return respostaServidor(res, "Login bem-sucedido!", 200);
-});
-
-app.get("/esqueciSenha", (req, res) => {
-  const { email, codigoVer }: EsqueciSenhaInterface = req.body;
-
-  let encontreiSenha = false;
-  for (let i of cadastros) {
-    if (i.email === email && i.codigo == codigoVer) {
-      encontreiSenha = true;
-    }
-  }
-
-  respostaServidor(res, encontreiSenha, 200);
-});
-
-app.post("/efetuarCadastro", (req, res) => {
-  const { email, senha, username, telefone }: CadastroInterface = req.body;
-
-  // Validar e-mail
-  const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!emailValido.test(email)) {
-    return respostaServidor(res, "E-mail inválido!", 400);
-  }
-
-  // Validar senha
-  const senhaValida = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
-
-  if (!senhaValida.test(senha)) {
-    return respostaServidor(res, "A senha deve ter no mínimo 8 caracteres, uma letra e um número.", 400);
-  }
-
-  return respostaServidor(res, "Cadastro realizado com sucesso!", 200);
-});
-
 app.get("/", (req, res) => {
   res.send("Servidor Node.js com TypeScript funcionando!");
 });
-
